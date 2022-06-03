97f25ba88f3a60e3453eb1477eaa8872ac942d6d
NahlaAyoub <44461051+NahlaAyoub@users.noreply.github.com>
2022-06-03 10:05:28 +0200

    editing my name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,6 +35,8 @@
                     @endauth
                 </div>
             @endif
+            
+            <h1 style="text-align:center ;">مرحباً انا نهلة ايوب</h1>
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
